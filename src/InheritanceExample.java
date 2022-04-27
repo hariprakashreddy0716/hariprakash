9494d49892913a80988aa415099a34c6c7c1b77c
hariprakashreddy0716 <104200276+hariprakashreddy0716@users.noreply.github.com>
2022-04-27 20:39:22 +0530

    Update InheritanceExample.java

--- src/InheritanceExample.java
+++ src/InheritanceExample.java
@@ -25,20 +25,14 @@ public class InheritanceExample extends C {
         System.out.println(args[0]+"  "+args[1]);
         C obj = new C();
         obj.add(3,4);
-//      obj.disaplay();
-        obj.show();// this function calling from same class no inheritance
+        obj.show();
 
         InheritanceExample obj1 = new InheritanceExample();
-        obj1.show(); // this function calling from super class this is inheritance
+        obj1.show(); 
         obj1.add(3,6);
         obj1.show2();
-
-//        you can call only function which are available in C class.
-//        function which not available C but available in child class can not call that function
-//        if you want  to call function that function should be available in parent class then only u can call
         C obj3 = new InheritanceExample();
-        obj3.add(7,8);  //sub class
-//        obj3.show2();
+        obj3.add(7,8); 
         obj3.show();
 
     }
