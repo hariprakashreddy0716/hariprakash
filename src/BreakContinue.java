f793d7b158cf31affdf43cfe98619830809d7d91
hariprakashreddy0716 <104200276+hariprakashreddy0716@users.noreply.github.com>
2022-04-27 20:47:19 +0530

    Update BreakContinue.java

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -3,7 +3,7 @@ public class BreakContinue {
         for(int i=0;i<=10;i++){
             if(i==5){
                 break;
-//           continue;
+              continue;
             }
             System.out.println("i = " + i);
         }
